d15b8ffde3cfb1e1c0e53266dc6d43a0a72df493
zzZehran <zwhranfarooq24@gmail.com>
2025-04-16 16:58:47 +0530

    added use() to act more like express

--- app.js
+++ app.js
@@ -2,42 +2,52 @@ import { initialize } from "./index.js";
 
 const app = initialize();
 
-app.get("/", (req, res) => {
-  console.log("/");
-  res.end("From /");
+app.use("/use", (req, res, next) => {
+  console.log("USE");
+  next();
+  res.end("From .use /");
 });
+app.post("/use/:id", (req, res) => {
+  console.log("/use/zehran");
+});
+
+// app.get("/", (req, res) => {
+//   console.log("/");
+//   res.end("From /");
+// });
+
 // routes for next()
-app.get("/next/*spat", (req, res, next) => {
-  console.log("0");
-  next();
-  console.log("1");
-  res.end("Next() is working!");
-});
-app.get("/next/user", (req, res, next) => {
-  console.log("2");
-  next();
-  console.log("3");
-});
-app.get("/next/user", (req, res) => {
-  console.log("4");
-});
+// app.get("/next/*spat", (req, res, next) => {
+//   console.log("0");
+//   next();
+//   console.log("1");
+//   res.end("Next() is working!");
+// });
+// app.get("/next/user", (req, res, next) => {
+//   console.log("2");
+//   next();
+//   console.log("3");
+// });
+// app.get("/next/user", (req, res) => {
+//   console.log("4");
+// });
 
 //post routes
-app.post("/post", async (req, res) => {
-  console.log("POST req.");
-  let body = [];
-  req
-    .on("data", (chunk) => {
-      body.push(chunk);
-    })
-    .on("end", () => {
-      body = Buffer.concat(body).toString();
-      // at this point, `body` has the entire request body stored in it as a string
-      console.log(body);
-    });
+// app.post("/post", async (req, res) => {
+//   console.log("POST req.");
+//   let body = [];
+//   req
+//     .on("data", (chunk) => {
+//       body.push(chunk);
+//     })
+//     .on("end", () => {
+//       body = Buffer.concat(body).toString();
+//       // at this point, `body` has the entire request body stored in it as a string
+//       console.log(body);
+//     });
 
-  res.end("<h1>/POST route</h1>");
-});
+//   res.end("<h1>/POST route</h1>");
+// });
 
 const port = 3000;
 app.listen(port, () => {
--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import http from "node:http";
 import mergeDescriptors from "merge-descriptors";
 import url from "node:url";
+import path from "path/posix";
 import { match } from "path-to-regexp";
+
+const USE_METHOD_STRING = "__USE";
 
 export function initialize() {
   const routes = [];
@@ -13,31 +16,64 @@ export function initialize() {
     post: (path, handlerFn) => {
       routes.push({ path, handlerFn, method: "POST" });
     },
+    use: (pathOrHandler, handlerFn) => {
+      if (typeof pathOrHandler === "function") {
+        routes.push({
+          path: "*spat",
+          handlerFn: pathOrHandler,
+          method: USE_METHOD_STRING,
+        });
+      } else if (
+        typeof pathOrHandler === "string" &&
+        typeof handlerFn === "function"
+      ) {
+        pathOrHandler =
+          pathOrHandler[pathOrHandler.length - 1] === "/"
+            ? pathOrHandler.slice(0, pathOrHandler.length - 1)
+            : pathOrHandler;
+        routes.push({
+          path: pathOrHandler + "{/*spat}",
+          handlerFn,
+          method: USE_METHOD_STRING,
+        });
+      }
+    },
   };
 
   function matchRoutes(urlPathname, urlMethod) {
-    return routes.filter((route) => {
-      const matchFn = match(route.path);
-      const isMatch = matchFn(urlPathname);
-      return isMatch && urlMethod === route.method;
-    });
+    return routes
+      .filter((route) => {
+        const matchFn = match(route.path);
+        const isMatch = matchFn(urlPathname);
+        return (
+          isMatch &&
+          (urlMethod === route.method || route.method === USE_METHOD_STRING)
+        );
+      })
+      .map((route) => {
+        //read again
+        const matchFn = match(route.path);
+        const isMatch = matchFn(urlPathname);
+        return { route, params: isMatch.params };
+      });
   }
 
   const server = http.createServer((req, res) => {
     const method = req.method;
     const path = url.parse(req.url).pathname;
-
     // console.log(method, path);
 
-    const matchedRoutes = matchRoutes(path, method);;
-      let i = 0;
-      let handlerFn;
-      function next() {
-        handlerFn = matchedRoutes[i]?.handlerFn;
-        i += 1;
-        typeof handlerFn === "function" && handlerFn(req, res, next);
-      }
-      next();
+    const matchedRoutes = matchRoutes(path, method);
+    console.log("Matched routes:", matchedRoutes);
+    let i = 0;
+    let handlerFn;
+    function next() {
+      handlerFn = matchedRoutes[i]?.route?.handlerFn;
+      i += 1;
+      req.params = matchedRoutes[i]?.params;
+      typeof handlerFn === "function" && handlerFn(req, res, next);
+    }
+    next();
   });
 
   return mergeDescriptors(server, lib);
